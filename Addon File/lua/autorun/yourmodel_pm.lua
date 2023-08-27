fff10c5c8592dfd0e0bcbd0fe873d20bcdbd9862
epicgoat44 <143351650+epicgoat44@users.noreply.github.com>
2023-08-27 15:10:33 -0400

    Add files via upload

---
+++ Addon File/lua/autorun/yourmodel_pm.lua
@@ -0,0 +1,3 @@
+player_manager.AddValidModel( "Joey", "models/joe_herrick/joseph_herrick/Joey_pm.mdl" )
+list.Set( "PlayerOptionsModel", "Joey", "models/joe_herrick/joseph_herrick/Joey_pm.mdl" )
+//player_manager.AddValidHands( "Joey", "models/your_name/character_name/your_model_pm_arms", 0, "00000000" )
